66a9e2719683a8f4da3210674b11e8b6c28e09c2
Ahmad-Elkhawaldeh1 <71396577+Ahmad-Elkhawaldeh1@users.noreply.github.com>
2020-10-19 19:36:17 -0400

    Add files via upload

---
+++ unit2_05.ino
@@ -0,0 +1,35 @@
+
+Moves servo from 0 to 180 and back 
+created by: Ahmad El-khawaldeh
+created on: oct 17
+
+
+#include <Servo.h>
+
+
+Servo Servo;
+int angle= 10;
+
+void setup() {
+  Servo.attach(8);
+  Servo.write(angle);
+  
+}
+
+void loop()
+{
+  for(angle =10; angle< 180; angle++)
+ {
+   Servo.write(angle);
+   delay(15);
+ }
+ 
+ 
+   for(angle = 180; angle > 10; angle--)
+   {
+     Servo.write(angle);
+     delay(15);
+   }
+ }
+ 
+
